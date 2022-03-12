71060223f378163d5a0e8de00a8b171caa3fea04
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-03-12 02:41:58 -0300

    mais exemplos resolvidos

--- fundamentos/javascript/dia04/scripts.js
+++ fundamentos/javascript/dia04/scripts.js
@@ -100,3 +100,84 @@
 //     player.medals.silver +
 //     " medalhas de prata"
 // );
+
+// falta completar
+// let info = {
+//   personagem: "Margarida",
+//   origem: "Pato Donald",
+//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+// };
+// // console.log("Bem vinda, " + info.personagem);
+// info["recorrente"] = "Sim";
+// // console.log(info);
+// // for (let key in info) {
+// //   console.log(info[key]);
+// //}
+// let object = {
+//   personagem: "Tio Patinhas",
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: "O último MacPatinhas",
+//   recorrente: "Sim",
+// };
+// for (let chave in info) {
+//   console.log(info[chave]);
+// }
+
+// Exemplo  6  resolvido
+// let leitor = {
+//   nome: "Julia",
+//   sobrenome: "Pessoa",
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: "O Pior Dia de Todos",
+//       autor: "Daniela Kopsch",
+//       editora: "Tordesilhas",
+//     },
+//   ],
+// };
+// // console.log(
+// //   "O livro favorito de " +
+// //     leitor.nome +
+// //     " " +
+// //     leitor.sobrenome +
+// //     " se chama " +
+// //     leitor.livrosFavoritos[0].titulo
+// // );
+// // adicionar mais um livro favorito na chave livrosFavoritos que é um array com um objeto
+
+// leitor.livrosFavoritos.push({
+//   titulo: "Harry Potter e o Prisioneiro de Azkaban",
+//   autor: "JK Rowling",
+//   editora: "Rocco",
+// });
+// console.log(
+//   leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos"
+// );
+
+// let names = {
+//   person1: "João",
+//   person2: "Maria",
+//   person3: "Jorge",
+// };
+// for (let name in names) {
+//   console.log("Olá " + names[name]);
+// }
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+// for (let index in cars) {
+//   console.log(cars[index]);
+// }
+
+// ----------------------------------------------------
+// Parte 2 - Functions
+
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for. RESOLVIDO
+function isPalindrome(string) {
+  if (string.split("").reverse().join("") === string) {
+    return true;
+  }
+  return false;
+}
+
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
